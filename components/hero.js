eb52ac16765d7f55900cf4efbdae8afd1e38bf87
simonachess <simonachess@gmail.com>
2026-02-20 16:19:23 +0200

    margin fixes

--- components/hero.js
+++ components/hero.js
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="w-full flex flex-col py-[120px] px-[30px] items-center bg-[url(/hero-section@3x.png)] bg-cover bg-no-repeat bg-[top] text-center text-33xl text-gray-white font-body-regular-400">
+    <div className="w-full flex flex-col py-[120px] items-center bg-[url(/hero-section@3x.png)] bg-cover bg-no-repeat bg-[top] text-center text-33xl text-gray-white font-body-regular-400">
       <div className="w-full flex flex-col items-center justify-center gap-[62px]">
         <div className="flex flex-col items-center gap-20 max-w-[720px]">
           <div className="relative leading-[72px] font-semibold">
