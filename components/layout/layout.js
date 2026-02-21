9d26ddf1cbe6357fb5d47a69876c2d6716b79a22
simonachess <simonachess@gmail.com>
2026-02-21 23:29:31 +0200

    small fix

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,7 +5,9 @@ export default function Layout({ children }) {
   return (
     <div className="relative bg-gray-white w-full flex flex-col items-center justify-start">
       <Header />
-      {children}
+      <main id="main-content" className="flex-1 w-full">
+        {children}
+      </main>
       <Footer />
     </div>
   );
